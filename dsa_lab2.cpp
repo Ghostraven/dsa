efd47832e1a2fc16da793b958bed362317e74f7a
MarvoloGrim <al3x95x@gmail.com>
2015-03-06 15:39:51 +0200

    Create dsa_lab2.cpp

---
+++ dsa_lab2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+int temp;
+
+class Complex {
+private:
+double a;
+double b;
+
+public:
+
+    Complex();
+
+    Complex(double theA, double theB) {
+        this -> a = theA;
+        this -> b = theB;
+    };
+    
+    static Complex addition(Complex &nrOne, Complex &nrTwo) {
+        Complex result(0,0);
+        result.a = nrOne.a + nrTwo.a;
+        result.b = nrOne.b + nrTwo.b;
+        return result;
+    }
+    
+    static Complex division(Complex &nrOne, Complex &nrTwo) {
+        Complex result(0,0);
+        result.a = (nrOne.a * nrTwo.a + nrOne.b * nrTwo.b) / ((nrTwo.a * nrTwo.a) + (nrTwo.b * nrTwo.b));
+        result.b = (nrOne.b * nrTwo.a - nrTwo.b * nrOne.a) / ((nrTwo.a * nrTwo.a) + (nrTwo.b * nrTwo.b));
+        return result;
+    }
+    
+    static Complex multiplication(Complex &nrOne, Complex &nrTwo) {
+        Complex result(0,0);
+        result.a = nrOne.a * nrTwo.a - (nrOne.b * nrTwo.b);
+        result.b = nrOne.a * nrTwo.b + nrTwo.a * nrOne.b;
+        return result;
+    }
+    
+    void printNumber() {
+        cout << a << " + i" << b;
+    }
+};
+
+
+void bubbleSort(int (&array)[5]) {
+    bool changed;
+    do {
+        changed = false;
+        for (int i = 0 ; i < 4; i++) {
+            if (array[i] > array[i+1]) {
+                swap(array[i], array[i+1]);
+                changed = true;
+            }
+        }
+    } while (changed == true);
+}
+
+int main() {
+    int array[5] = {6, 5, 4, 3, 2};
+    for (int i = 0; i < 5; i++) {
+        cout << array[i] << ' ';
+    }
+    bubbleSort(array);
+    cout << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << array[i] << ' ';
+    }
+    cout << endl;
+    Complex firstNumber(5,6);
+    Complex secondNumber(8, 9);
+    firstNumber.printNumber();
+    cout << endl;
+    secondNumber.printNumber();
+    cout << endl;
+    Complex added(0,0);
+    Complex divided(0,0);
+    Complex multiplied(0,0);
+    added = Complex::addition(firstNumber, secondNumber);
+    divided = Complex::division(firstNumber, secondNumber);
+    multiplied = Complex::multiplication(firstNumber, secondNumber);
+    added.printNumber();
+    cout << endl;
+    divided.printNumber();
+    cout << endl;
+    multiplied.printNumber();
+    return 0;
+}
